f91fefb02c9a8d1069cc600159abd95ea7b2a390
Déw <burakonturk2000@gmail.com>
2023-06-17 04:52:54 +0300

    .

---
+++ Assets/Waypoints.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        foreach(Transform t in transform)
+        {
+            Gizmos.color
+        }
+    }
+}
