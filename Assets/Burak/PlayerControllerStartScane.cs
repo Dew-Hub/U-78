1bd4608cb5a9d3708f5d2dbed459d94cd7da2e1e
Déw <burakonturk2000@gmail.com>
2023-06-17 04:33:43 +0300

    .

---
+++ Assets/Burak/PlayerControllerStartScane.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerStartScane : MonoBehaviour
+{
+    //private CharacterController controller;
+
+    private Vector3 playerVelocity;
+    private bool grounded;
+    public float gravity = -9.8f;
+    public Rigidbody rb;
+    private bool isGrounded = true;
+    public float jumpForce = 5f;
+
+    public float speed;
+    private Vector2 move, mouseLook;
+    private Vector3 rotationTarget;
+    public bool isPc;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+        mouseLook = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Jump();
+    }
+
+    
+
+    void Start()
+    {
+            //controller = GetComponent<CharacterController>();
+    }
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //grounded = controller.isGrounded;
+        if (isPc)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                rotationTarget = hit.point;
+            }
+            movePlayerWithAim();
+        }        
+    }
+
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            // Zıplama kuvvetini uygula
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Karakter yerdeyken Collision oluştuysa isGrounded'i true yap
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // Karakter havadaysa isGrounded'i false yap
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    public void movePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        if(movement != Vector3.zero)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    public void movePlayerWithAim()
+    {
+        if (isPc)
+        {
+            var lookPos = rotationTarget - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+
+            Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
+
+            if (aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+            }
+        }
+
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
