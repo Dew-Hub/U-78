6cf0abdd357473992e798121db48568c532929d4
Déw <burakonturk2000@gmail.com>
2023-06-17 21:03:25 +0300

    .

---
+++ Assets/Burak/GRAVIGUN/pickUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour
+{
+    public float pickUpRange = 5;
+    private GameObject heldObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
