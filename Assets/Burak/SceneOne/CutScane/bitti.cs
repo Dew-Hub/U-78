af732cee2f715ce9148874e1719007edc03de858
Déw <burakonturk2000@gmail.com>
2023-07-14 05:19:26 +0300

    .

---
+++ Assets/Burak/SceneOne/CutScane/bitti.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bitti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("E tuşuna basıldı.");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
