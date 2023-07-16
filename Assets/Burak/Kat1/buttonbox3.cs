b9db92c78b4463317c75728d76445e2349db86e9
Déw <burakonturk2000@gmail.com>
2023-07-16 16:18:36 +0300

    .

---
+++ Assets/Burak/Kat1/buttonbox3.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonbox3 : MonoBehaviour
+{
+    public celikkapiacilis celikkapi;
+    private bool durum = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("box"))
+        {
+            durum = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("box"))
+        {
+            durum= false;
+        }
+    }
+
+    private void Update()
+    {
+        celikkapi.box2 = durum;
+    }
+
+
+}
