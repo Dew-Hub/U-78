2b4e41b154da8d12761293d6b5cbc1e2c0028c76
Déw <burakonturk2000@gmail.com>
2023-07-17 22:03:30 +0300

    .

---
+++ Assets/Burak/NextSceneOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneOnCollision : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
