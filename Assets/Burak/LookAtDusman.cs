3a200e732bbf32bfeb84032eaed4caaef38f5608
Déw <burakonturk2000@gmail.com>
2023-07-17 21:36:17 +0300

    .

---
+++ Assets/Burak/LookAtDusman.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtDusman : MonoBehaviour
+{
+    private Transform target;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        // Düşmanın karakterin bulunduğu konuma bakması
+        Vector3 lookDirection = target.position - transform.position;
+        lookDirection.y = 0f; // Y eksenindeki rotasyonu sıfırla (düşmanın yüksekliğini korur)
+        transform.rotation = Quaternion.LookRotation(lookDirection);
+    }
+
+}
